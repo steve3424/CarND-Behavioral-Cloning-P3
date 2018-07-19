0a516a8f5845ebb3a1ddf7f4a1cc861614f984b5
steve3424 <30740106+steve3424@users.noreply.github.com>
2018-07-18 20:31:12 -0400

    increase drive.py speed, add code to load multiple data folders train_model.py

--- train_model.py
+++ train_model.py
@@ -2,38 +2,52 @@ import csv
 import cv2
 import numpy as np
 
-lines = []
-with open('../first_track_data/driving_log.csv') as csvfile:
-	reader = csv.reader(csvfile)
-	for line in reader:
-		lines.append(line)
 
-correction = 0.1
+data_folders = ['track1_lap1_data', 'track1_lap2_data']
+
+csv_lines = []
+for folder in data_folders:
+	with open(folder + '/driving_log.csv') as csvfile:
+		reader = csv.reader(csvfile)
+		lines = []
+		for line in reader:
+			lines.append(line)
+		csv_lines.append(lines)
+
+
+
+correction = 0.2
 corrections = [0, correction, -correction]
 
 images = []
 measurements = []
-for line in lines:
-	for i in range(3):
-		source_path = line[i]
-		filename = source_path.split('\\')[-1]
-		current_path = '../first_track_data/IMG/' + filename
-		image = cv2.imread(current_path)
-		images.append(image)
-		measurement = float(line[3]) + corrections[i]
-		measurements.append(measurement)
+for f in range(len(data_folders)):
+	folder = data_folders[f]
+	for line in csv_lines[f]:
+		for i in range(3):
+			source_path = line[i]
+			filename = source_path.split('\\')[-1]
+			current_path = folder + '/IMG/' + filename
+			image = cv2.imread(current_path)
+			images.append(image)
+			measurement = float(line[3]) + corrections[i]
+			measurements.append(measurement)
 
-flipped_images, flipped_measurements = [], []
+
+augmented_images, augmented_measurements = [], []
 for image, measurement in zip(images,measurements):
-	flipped_images.append(image)
-	flipped_measurements.append(measurement)
-	flipped_images.append(cv2.flip(image,1))
-	flipped_measurements.append(measurement * -1.0)
+	augmented_images.append(image)
+	augmented_measurements.append(measurement)
+	augmented_images.append(cv2.flip(image,1))
+	augmented_measurements.append(measurement * -1.0)
 
-X_train = np.array(flipped_images)
-y_train = np.array(flipped_measurements)
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
+print(X_train.shape)
+print(y_train.shape)
 
+'''
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
@@ -58,9 +72,9 @@ model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
 
+
 model.save('model.h5')
-
-
+'''
 '''
 LENET ARCH
 model.add(Convolution2D(6,5,5,activation='relu'))
@@ -73,4 +87,14 @@ model.add(Flatten())
 model.add(Dense(120))
 model.add(Dense(84))
 model.add(Dense(1))
+'''
+
+
+'''
+TECHNIQUES
+
+-Build robust network (Nvidia's model)
+-Drive multiple laps
+-Train on all 3 camera images with correction
+-Use flipped images (same as driving backward lap??)
 '''
